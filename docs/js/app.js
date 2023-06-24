bd32bbee17e509cf01767f40ac591813709c0715
Jon Nyman <nymanjon@gmail.com>
2023-06-23 22:05:51 -0700

    init

---
+++ docs/js/app.js
@@ -0,0 +1,54 @@
+(() => {
+
+let $count = document.getElementById("count")
+function updateCount() {
+    $count.textContent =
+        Array.from(document.querySelectorAll('tr:not([hidden])')).length - 1
+}
+
+let $elevation = document.getElementById("elevation")
+$elevation.addEventListener("change", filter)
+
+let $search = document.getElementById("search")
+$search.addEventListener("input", filter)
+
+let $start = document.getElementById("start")
+$start.addEventListener("change", filter)
+
+let $end = document.getElementById("end")
+$end.addEventListener("change", filter)
+
+let $reset = document.getElementById("reset")
+$reset.addEventListener("click", () => {
+    $elevation.value = ""
+    $search.value = ""
+    $start.value = ""
+    $end.value = ""
+    filter()
+})
+
+function filter() {
+    let vegetable = $search.value?.trim().toLowerCase(),
+        elevation = $elevation.value,
+        start = $start.value.slice(-5),
+        end = $end.value.slice(-5),
+        header = true
+    for (let row of document.querySelectorAll(`tr`)) {
+        if (header) { header = false; continue }
+        let dVegetable = row.dataset.vegetable,
+            dElevation = row.dataset.elevation,
+            dStart = row.dataset.start,
+            dEnd = row.dataset.end
+        row.hidden = 
+            !((!dVegetable || dVegetable.includes(vegetable))
+            && (!elevation || !dElevation || dElevation === elevation)
+            && (!start || !dStart || dStart >= start)
+            && (!end || !dEnd || dEnd <= end))
+    }
+    updateCount()
+}
+
+filter()
+
+})()
+
---
+++ tasks/start.nu
@@ -0,0 +1,46 @@
+#!/usr/bin/env nu
+
+let rawData = (open data.csv)
+
+let data = (
+$rawData
+| each { |x|
+    let start = (
+        $x.start
+        | date format
+        | str substring 5..11
+        | split row ' '
+        | $"($in.1) ($in.0)")
+    let end = (
+        $x.end
+        | date format
+        | str substring 5..11
+        | split row ' '
+        | $"($in.1) ($in.0)")
+    $'<tr data-vegetable="($x.vegetable | str downcase)" data-elevation="($x.elevation)" data-start="($x.start)" data-end="($x.end)">
+        <td>($x.vegetable)</td>
+        <td>(if $x.elevation == 3000 {
+             "3000 - 4500"
+         } else if $x.elevation == 4500 {
+             "4500 - 6000"
+         } else {
+            "Above 6000"
+         })</td>
+        <td>($start)</td>
+        <td>($end)</td>
+    </tr>'
+}
+| str join '')
+
+let vegetables = (
+$rawData
+| get vegetable
+| sort | uniq
+| each { |it| $'<option value="($it)"></option>' }
+| str join '')
+
+open ./src/index.html
+| str replace '{{data}}' $data
+| str replace '{{vegetables}}' $vegetables
+| save -f ./docs/index.html
+
